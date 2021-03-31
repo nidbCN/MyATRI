89b499d9ac3c4dfe8a73d22d1b6334215357d837
Gaein nidb <mail@gaein.cn>
2021-03-31 22:06:43 +0800

    Add data

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
+# coding=utf-8
+
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 
-chatbot = ChatBot('Ron Obvious')
+chatbot = ChatBot('ATRI')
 
-# Create a new trainer for the chatbot
 trainer = ChatterBotCorpusTrainer(chatbot)
 
-# Train the chatbot based on the english corpus
-trainer.train("chatterbot.corpus.english")
+trainer.train(
+    "chatterbot.corpus.atri"
+)
 
 # Get a response to an input statement
-chatbot.get_response("Hello, how are you today?")
+while True:
+    msg = input()
+    r = chatbot.get_response(msg)
 
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("RESPONSE:" + str(r))
